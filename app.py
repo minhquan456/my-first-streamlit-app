f13d8a3a429b52d3c5b4457b606563e638878e51
minhquan456 <127492934+minhquan456@users.noreply.github.com>
2023-03-10 23:40:57 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title('Giải phương trình bậc nhất')
+
+number1 = st.number_input('Tham số a')
+number2 = st.number_input('Tham số b')
+
+if st.button('Giải'):
+  st.write('phương trình có 1 nghiệm', -number2/number1)
